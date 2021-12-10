f9e3b167224bf76d797f9196e04cacacea290398
Patrick Brennan <pbrennanmusic@gmail.com>
2021-12-10 14:36:40 -0500

    commit tree 427b33bd69abd06d657b349accd21ad51edc31a7

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>
+        Movies Page
+      </h1>
+      {movies.map((movie) => (
+        <div className="movie">
+          <h2>{movie.title}</h2>
+          <p>{movie.time}</p>
+          <p>{movie.metascore}</p>
+          <ul>
+            {movie.genres.map((genre) => (
+              <li>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
